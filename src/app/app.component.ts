fb2e45013d62987e5e22086528f59720af4d2487
Daniel Sodré <danielsodre2020@gmail.com>
2024-11-06 08:03:44 -0300

    Crinado rotas, breve validação do login e criando pasta de imagens no projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FeedComponent } from "./components/feed/feed.component";
+import { LoginComponent } from './components/login/login.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FeedComponent],
+  imports: [RouterOutlet, FeedComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../usuario';
+import { provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +13,27 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usuarios: Usuario [] = []
+  email: String = "";
+  senha: String= "";
 
-  constructor(private rota: Router){
-  }
+  constructor(private rota: Router, private  usuarioService: UsuarioService){}
 
-  login(){
-    this.rota.navigate(['/feed']);
-  }
+  ValidarLogin(){
+    this.usuarioService.getUsuarios().subscribe((dado) =>{
+      this.usuarios = dado;
+
+      this.usuarios.forEach(usuario => {
+        if(usuario.login == this.email && usuario.senha == this.senha){
+          this.rota.navigate(['/feed']);
+        }else{
+          alert("Erro");
+        }
+      });
+
+    }
+
+
+  )}
 
 }
