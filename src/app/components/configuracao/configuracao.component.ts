b0fcaff0eb9bbc0a3458422729e8781166d3c850
Daniel Sodré <danielsodre2020@gmail.com>
2024-11-21 14:36:00 -0300

    Estilização da navbar e siderbar left atualizada. Footer criado e estilizado. Templete da tela de configuração atualizado. Validação de login com email ou usuario finalizada.

--- src/app/components/configuracao/configuracao.component.ts
+++ src/app/components/configuracao/configuracao.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { SidebarLeftComponent } from "../sidebar/sidebar-left/sidebar-left.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-configuracao',
   standalone: true,
-  imports: [NavbarComponent, SidebarLeftComponent],
+  imports: [NavbarComponent, SidebarLeftComponent, FooterComponent],
   templateUrl: './configuracao.component.html',
   styleUrl: './configuracao.component.css'
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,37 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Usuario } from '../../usuario';
 import { AuthserviceService } from '../../services/authService/authservice.service';
 import { UsuarioService } from '../../services/usuario/usuario.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   usuarios: Usuario[] = [];
-  email: String = "";
+  login: String = "";
   senha: String = "";
+  isLoginInvalid: boolean = false;
 
   constructor(private rota: Router, private usuarioService: UsuarioService, private authService: AuthserviceService) { }
 
 
-  ValidarLogin() {
+  validarLogin() {
     this.usuarioService.getUsuarios().subscribe((dado) => {
       this.usuarios = dado;
 
       for (const usuario of this.usuarios){
-        if (usuario.login == this.email && usuario.senha == this.senha) {
+        if ( (usuario.login == this.login || usuario.email == this.login) && usuario.senha == this.senha) {
           this.authService.login(usuario);//Enviando token de usuario logado
           this.rota.navigate(['/feed']);
           return;
         }
       }
-      alert('Usuario invalido');
+      this.isLoginInvalid = true;
     })
   }
 
