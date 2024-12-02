99c35618934f9133a71e93f918db41fc17112c14
DANIEL SODRE DE SOUZA PAULA <1A280932978@fumecacad.local>
2024-12-02 09:16:14 -0300

    Passa o conteudo de excluir que estava em atualizarDados para o exlcuirConta, Estiliza e implementa politica de excluir conta em excluirConta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { PerfilComponent } from './components/perfil\'s/perfil/perfil.component'
 import { CadastroComponent } from './components/cadastro\'s/cadastro/cadastro.component';
 import { LoginComponent } from './components/login\'s/login/login.component';
 import { AtualizaDadoComponent } from './components/atualizaDado\'s/atualizaDado/atualizaDado.component';
+import { ExcluirContaComponent } from './components/excluirContas/excluir-conta/excluir-conta.component';
 
 export const routes: Routes = [
   {path: 'feed', component: FeedComponent, canActivate: [AuthGuard]},
   { path: '', component: LoginComponent },
   {path : 'cadastro', component: CadastroComponent},
   {path : 'atulizarDados', component: AtualizaDadoComponent, canActivate: [AuthGuard]},
+  {path : 'excluirConta', component: ExcluirContaComponent, canActivate: [AuthGuard]},
   {path : 'publicacao', component: PublicacaoComponent},
   {path : 'perfil', component: PerfilComponent, canActivate: [AuthGuard]}
 ]
--- src/app/components/excluirContas/excluir-conta/excluir-conta.component.ts
+++ src/app/components/excluirContas/excluir-conta/excluir-conta.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../navbar/navbar.component";
+import { SidebarLeftComponent } from "../../sidebar/sidebar-left/sidebar-left.component";
+import { atualizaDadodadosComponent } from "../../atualizaDado's/atualizaDado-dados/atualizaDado-dados.component";
+import { ExcluirContaDadosComponent } from "../excluir-conta-dados/excluir-conta-dados.component";
 
 @Component({
   selector: 'app-excluir-conta',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, SidebarLeftComponent, atualizaDadodadosComponent, ExcluirContaDadosComponent],
   templateUrl: './excluir-conta.component.html',
   styleUrl: './excluir-conta.component.css'
 })
